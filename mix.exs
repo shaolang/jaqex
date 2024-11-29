22109f3b10752a7d8c0cd210c7eb36c19380e7bb
shaolang <shaolang@ymail.com>
2024-11-29 12:19:38 +0800

    Add package and doc metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,14 +1,20 @@
 defmodule Jaqex.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/shaolang/jaqex"
+  @version "0.1.0"
+
   def project do
     [
       app: :jaqex,
       deps: deps(),
+      description: "Wrapper for Jaq (a jq clone focused on correctness, speed, and simplicity)",
+      docs: docs(),
       elixir: "~> 1.16",
-      source_url: "https://github.com/shaolang/jaqex",
+      package: package(),
+      source_url: @source_url,
       start_permanent: Mix.env() == :prod,
-      version: "0.1.0"
+      version: @version
     ]
   end
 
@@ -20,6 +26,26 @@ defmodule Jaqex.MixProject do
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:rustler, "~> 0.35", optional: true, runtime: false},
       {:rustler_precompiled, "~> 0.8"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "README",
+      name: "Jaqex",
+      source_ref: "v#{@version}",
+      canonical: "https://hexdocs.pm/jaqex",
+      source_url: @source_url,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Shaolang Ai"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/shaolang/jaqex"},
+      files: ["lib", "mix.exs", "README.md", "navtive", "checksum-*.exs"],
     ]
   end
 end
